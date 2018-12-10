6830c8f66c9136926c9b8c2d9ad66bcbb86176aa
duycv <caovanduy15gmail.com>
2018-12-10 16:01:40 +0700

    correct alignment

--- app/Controller/ChatController.php
+++ app/Controller/ChatController.php
@@ -1,32 +1,35 @@
 <?php 
 class ChatController extends AppController {
-	public $uses = "tFeed";
+	public $uses = array("tFeed", "tUser");
 
 	public function index() {
 
 	}
 
 	public function feed() {
-		// get data from database and display data and sort desc
+		// get data from database and sort desc
 		$data = $this->tFeed->find('all', array(
- 					'order'=>array('id DESC')));
+					'order'=>array('id DESC')));
+
+		// Transfer data to view
 		$this->set("data", $data);
 
 		// get data from form and save data
 		if ($this->request->is('post')) {
+			// get time
 			$today = date("Y-m-d H:i:s");
-			         // 2001-03-10 17:16:18 (the MySQL DATETIME format)
+			// 2001-03-10 17:16:18 (the MySQL DATETIME format)
 			$this->request->data['create_at'] = $today;
             $this->tFeed->create();
             if ($this->tFeed->save($this->request->data)) {
-                $this->Flash->success(__('Your post has been saved.'));
+                $this->Flash->success(__('Your message has been seen.'));
                 return $this->redirect(array('action' => 'feed'));
+            } else {
+            	$this->Flash->error(__('Unable to add your message.'));
             }
-            $this->Flash->error(__('Unable to add your post.'));
-        }
+    	}
+		
 	}
-
-
 }
 
  ?>
